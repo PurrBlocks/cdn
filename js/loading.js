b2c94d04d25a784166f121f334f4633c7d19440e
FelineAPI <felineapi@ro.ru>
2026-06-29 19:56:20 +0300

    Version 0.0.4

--- js/loading.js
+++ js/loading.js
@@ -1,1 +1,6 @@
-document.querySelector('pbi-root').innerHTML = '<pbi-loading-group class="pbi-loading-group"><pbi-loading class="pbi-loading"></pbi-loading></pbi-loading-group>';
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.querySelector('.pbi-root');
+  if (root) {
+    root.innerHTML = '<pbi-loading-group class="pbi-loading-group"><pbi-loading class="pbi-loading"></pbi-loading></pbi-loading-group>';
+  }
+});
--- js/theme.js
+++ js/theme.js
@@ -1,2 +1,3 @@
-let themeColor = document.documentElement.classList.contains('dark') ? 'dark' : 'light';
-document.body.className = 'body_' + themeColor;
+const themeColor = document.documentElement.classList.contains('dark') ? 'dark' : 'light';
+document.body.classList.remove('body_dark', 'body_light');
+document.body.classList.add('body_' + themeColor);
