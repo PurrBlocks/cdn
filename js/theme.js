6f63a8ade2a910856694be134ff9f95e537d7552
FelineAPI <felineapi@ro.ru>
2026-06-29 15:36:07 +0300

    Version 0.0.3

--- js/theme.js
+++ js/theme.js
@@ -1,3 +1,2 @@
 let themeColor = document.documentElement.classList.contains('dark') ? 'dark' : 'light';
 document.body.className = 'body_' + themeColor;
-document.getElementById('pbi-src-1').innerHTML = '<script src="https://purrblocks.github.io/cdn/js/theme.js"></script>'
